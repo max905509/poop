af6ea08e69a841ffeb086209c9c85bdd6e351d13
max905509 <max.3.14@yandex.ru>
2021-05-11 14:09:21 +0300

    fibonachi 2

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const answers = [];
+// var carName = "Ford";
+// var carYear = '2008';
 
-answers[0]=prompt("Q1", "");
-answers[1]=prompt("Q2", "");
-answers[2]=prompt("Q3", "");
+// function yearYear(year){return 10<10;}
 
+// yearYear(carYear)?console.log("true меньше 10"):console.log("false больше 10")
+// const answer = prompt("Возраст человека или машины?", "Человек");
+// answer == "Человек"? (alert("Человеку "+ (2021-answer) +"year")) : (alert("машина"));
+function fibonachi(inputString){
+    
+    var input = parseInt(inputString);
+    if (library[input]!= undefined){return library[input];}
+    for (var i = 2; i <=input; i++){
+        library[i+1]=library[i]+library[i-1];
+        if (i==input) {return library[i];}
+    }
+}
+
+
+var library = [0,1,1,2];
+var input = prompt("Какое по счету число фибоначи вы хотите?");
+console.log(fibonachi(input));
+alert(fibonachi(input));
